ba46c5157189547e218b52305efa74d7e48f602a
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-21 03:48:45 -0500

    test

--- update.sh
+++ update.sh
@@ -13,6 +13,7 @@ pihole_current=$(pihole -v | grep "Pi-hole" | cut -d ' ' -f 3)
 adminlte_current=$(pihole -v | grep "AdminLTE" | cut -d ' ' -f 6)
 pihole_ftl_current=$(pihole -v | grep "FTL" | cut -d ' ' -f 6)
 
+echo "Current Pi-hole version: $pihole_current"
 if [[ "$pihole_current" >= "$pihole_latest" ]] && [[ "$adminlte_current" >= "$adminlte_latest" ]] && [[ "$pihole_ftl_current" >= "$pihole_ftl_latest" ]] && [[ "$1" != "un" ]]; then
     echo "Pi-hole is already up to date."
     #exit 0
