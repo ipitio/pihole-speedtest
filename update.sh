7002f45a5b14639e53e2199e027dc6d501354543
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-21 11:58:37 -0500

    is it that

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ pihole_ftl_current=$(pihole -v | grep "FTL" | cut -d ' ' -f 6)
 
 if [[ ! "$pihole_current" < "$pihole_latest" ]] && [[ ! "$adminlte_current" < "$adminlte_latest" ]] && [[ ! "$pihole_ftl_current" < "$pihole_ftl_latest" ]] && [[ "$1" != "un" ]]; then
     echo "Pi-hole is already up to date"
-    exit 0
+    #exit 0
 fi
 
 curl -sSL https://github.com/ipitio/pihole-speedtest/raw/ipitio/uninstall.sh | tac | tac | sudo bash -s -- d
