99e1d073fdd980f7f8947af3d4ec9ef63dedf66f
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-25 21:46:26 -0500

    wb like this

--- install.sh
+++ install.sh
@@ -13,7 +13,23 @@ if [ "$1" != "un" ]; then
 	if [ ! -f /etc/apt/sources.list.d/ookla_speedtest-cli.list ]; then
 		echo "$(date) - Adding speedtest source..."
 		# https://www.speedtest.net/apps/cli
-		curl -sSLN https://github.com/ipitio/pihole-speedtest/raw/ipitio/ookla.sh | sudo bash
+		if [ -e /etc/os-release ]; then
+			. /etc/os-release
+
+			base="ubuntu debian"
+			os=${ID}
+			dist=${VERSION_CODENAME}
+
+			if [[ "${base//\"/}" =~ "${ID_LIKE//\"/}" ]]; then
+				os=${ID_LIKE%% *}
+				dist=${UBUNTU_CODENAME}
+				[ -z "$dist" ] && dist=${VERSION_CODENAME}
+			fi
+			echo "os=$os dist=$dist"
+			os=$os dist=$dist curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+		else
+			curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+		fi
 	fi
 	PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
 	apt-get install -y speedtest-cli- sqlite3 $PHP_VERSION-sqlite3 jq speedtest
