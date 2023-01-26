ba6416c052742793dfeb422103bf9ddaecd7e9b4
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-25 21:46:43 -0500

    word

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ if [ "$1" != "un" ]; then
 				dist=${UBUNTU_CODENAME}
 				[ -z "$dist" ] && dist=${VERSION_CODENAME}
 			fi
-			echo "os=$os dist=$dist"
+			echo "os=$os dist=$dist 1"
 			os=$os dist=$dist curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 		else
 			curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
