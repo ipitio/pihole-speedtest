0963c5677c3cb3c41b2ba09981a14373e4b79748
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-25 21:59:01 -0500

    bump

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ if [ "$1" != "un" ]; then
 				dist=${UBUNTU_CODENAME}
 				[ -z "$dist" ] && dist=${VERSION_CODENAME}
 			fi
-			echo "os=$os dist=$dist 4"
+			echo "os=$os dist=$dist 5"
 			wget -O /tmp/script.deb.sh https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh
 			sudo os=$os dist=$dist /tmp/script.deb.sh
 			rm -f /tmp/script.deb.sh
