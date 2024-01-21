ccae9a2778da6533c3cd4e8767165a437587578c
ipitio <21136719+ipitio@users.noreply.github.com>
2024-01-21 08:09:55 -0500

    set tags twice

--- mod.sh
+++ mod.sh
@@ -43,10 +43,12 @@ refresh() {
 			fi
 		fi
 	elif [ ! -z "$src" ]; then # revert
+		setTags $dest
 		git remote | grep -q upstream && git remote remove upstream
 		git remote add upstream $url
 		git fetch upstream -q
 		git reset --hard upstream/master
+		cd ..
 		setTags $dest
 		git -c advice.detachedHead=false checkout $latestTag
 	else # reset
